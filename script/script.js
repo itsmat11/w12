e9f8db6d84833586533a280067845ddf1f3239a6
Mattias <mattiasleungis@gmail.com>
2023-03-27 11:12:12 -0700

    done

--- script/script.js
+++ script/script.js
@@ -5,3 +5,30 @@ function alertUser() {
     buttonRef.removeEventListener('click', alertUser);
 }
 buttonRef.addEventListener('click', alertUser, {once : true});
+
+
+function changeBGPink() {
+    document.body.classList.add("pinkBG")
+}
+buttonRef.addEventListener('click', changeBGPink);
+
+function changeText() {
+    if (buttonRef.textContent === "Click Me!") {
+    buttonRef.textContent = "Clicked"
+}
+    else{
+        buttonRef.textContent = "Click Me!";
+    }
+}
+buttonRef.addEventListener('click', changeText);
+
+
+function updateImage() {
+    const image = document.querySelector("#shoppingCart");
+    image.setAttribute("src", "images/shopping .png");
+    image.setAttribute("widthh", 50);
+    image.setAttribute("height", 50);
+}
+
+buttonRef.addEventListener("click", updateImage);
+
